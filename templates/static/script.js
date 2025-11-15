af8ecdfad86fd4d833a284930fd42b71f27084f7
Bindhu <bindhusree564@gmail.com>
2025-11-15 15:09:04 +0530

    adding basic structure and checking functionality

---
+++ templates/static/script.js
@@ -0,0 +1,9 @@
+let del = document.getElementById('del')
+let edit = document.getElementById('edit')
+let like = document.getElementById('like')
+let new_post = document.getElementById('new_post')
+
+del.addEventListener("click",()=>{console.log("el deleted");})
+like.addEventListener("click",()=>{console.log("el liked");})
+edit.addEventListener("click",()=>{console.log("el edited");})
+new_post.addEventListener("click",()=>{console.log("new post created");})
